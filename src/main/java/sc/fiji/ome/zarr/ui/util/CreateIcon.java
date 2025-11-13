cba6b97787979e0ffb540e5124ae94a14fb7a016
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2025-11-13 11:38:26 +0100

    Refactor `CreateIcon` utility: add private constructor, improve URL validation with `URI`.

--- src/main/java/sc/fiji/ome/zarr/ui/util/CreateIcon.java
+++ src/main/java/sc/fiji/ome/zarr/ui/util/CreateIcon.java
@@ -1,21 +1,26 @@
 package sc.fiji.ome.zarr.ui.util;
 
+import java.net.URI;
 import java.net.URL;
 import java.awt.Image;
 import javax.swing.ImageIcon;
 
 public class CreateIcon {
-	public static ImageIcon fetchAndResizeIcon(String imageUrl) {
-		ImageIcon icon = null;
-		try {
-			icon = new ImageIcon( new ImageIcon(new URL(imageUrl))
-						.getImage()
-						.getScaledInstance(32, 32, Image.SCALE_SMOOTH)
-			);
-		} catch (Exception e) {
-			// Fallback to an empty image if the requested image can't be loaded
-			icon = new ImageIcon();
-		}
-		return icon;
-	}
+
+    private CreateIcon() {
+        // prevent instantiation
+    }
+
+    public static ImageIcon fetchAndResizeIcon(String imageUrl) {
+        try {
+            // Use URI for stricter validation and convert to URL
+            URL url = URI.create(imageUrl).toURL();
+            Image image = new ImageIcon(url).getImage()
+                    .getScaledInstance(32, 32, Image.SCALE_SMOOTH);
+            return new ImageIcon(image);
+        } catch (Exception e) {
+            // Fallback to an empty image if the requested image can't be loaded
+            return new ImageIcon();
+        }
+    }
 }
