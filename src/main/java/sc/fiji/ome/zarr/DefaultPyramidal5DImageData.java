f728217f3ef0a69c17be4f3279cfd26d439c9134
Vladimír Ulman <ulman@fi.muni.cz>
2025-11-13 21:12:19 +0100

    playing with exploration of metadatra

--- src/main/java/sc/fiji/ome/zarr/DefaultPyramidal5DImageData.java
+++ src/main/java/sc/fiji/ome/zarr/DefaultPyramidal5DImageData.java
@@ -42,8 +42,15 @@ import net.imglib2.EuclideanSpace;
 import net.imglib2.Volatile;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
+import org.janelia.saalfeldlab.n5.N5FSReader;
+import org.janelia.saalfeldlab.n5.N5Reader;
+import org.janelia.saalfeldlab.n5.ij.N5Importer;
+import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
 import org.janelia.saalfeldlab.n5.universe.N5DatasetDiscoverer;
+import org.janelia.saalfeldlab.n5.universe.N5Factory;
 import org.janelia.saalfeldlab.n5.universe.metadata.N5Metadata;
+import org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff.v04.OmeNgffMultiScaleMetadata;
+import org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff.v05.OmeNgffV05Metadata;
 import org.jetbrains.annotations.NotNull;
 import org.scijava.Context;
 
@@ -143,9 +150,28 @@ public class DefaultPyramidal5DImageData<
 		ijDataset.setName(imgPlus.getName());
 		ijDataset.setRGBMerged(false);
 	}
-	
-	public void REMOVEME() {
-		N5Metadata a = N5DatasetDiscoverer.discover(null).getMetadata();
+
+
+
+	public static void main(String[] args) {
+		//final String path = "/home/ulman/Documents/talks/CEITEC/2025_11_ZarrSymposium_Zurich/data/MitoEM_fixedRes.zarr/MitoEM_fixedRes";
+		final String path = "https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.5/idr0033A/BR00109990_C2.zarr/0";
+		final N5Reader n5reader = new N5Factory().openReader(path);
+		System.out.println("got reader: "+n5reader);
+		N5Metadata m = N5DatasetDiscoverer.discover(n5reader).getMetadata();
+		System.out.println("got metadata: "+m);
+
+		if (m instanceof OmeNgffV05Metadata) {
+			OmeNgffV05Metadata mv05 = (OmeNgffV05Metadata)m;
+			System.out.println("name: "+mv05.getName());
+			for (OmeNgffMultiScaleMetadata ms : mv05.multiscales) {
+				System.out.println("----------------");
+				System.out.println("name: "+ms.name+"     type: "+ms.type+"     version: "+ms.version);
+				System.out.println(ms.coordinateTransformations);
+			}
+		}
+
+
 		//TODO fetch v0.5 NGFF Metadata -> ask John
 		//there was supposed to be some class for it
 	}
