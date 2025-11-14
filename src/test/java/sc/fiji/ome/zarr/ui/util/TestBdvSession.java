abe5e60769ccadbd63054800879ec509ec90a701
Vladimír Ulman <ulman@fi.muni.cz>
2025-11-14 07:38:17 +0100

    added: ui/util/BdvSession for opening Bdv or adding to an existing one

---
+++ src/test/java/sc/fiji/ome/zarr/ui/util/TestBdvSession.java
@@ -0,0 +1,47 @@
+package sc.fiji.ome.zarr.ui.util;
+
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
+import java.util.Random;
+
+public class TestBdvSession {
+	public static void main(String[] args) {
+		//fake image
+		final Random rng = new Random();
+		final Img<UnsignedShortType> img = ArrayImgs.unsignedShorts(100, 100, 30);
+		img.forEach(p -> p.setReal(rng.nextFloat()));
+
+		BdvSession session = new BdvSession();
+		session.openNewBdv(img,"source 1");
+
+		int iters = 20;
+		while (iters > 0 && session.isBdvAlive()) {
+			try {
+				System.out.println("Seconds before adding a another image: "+iters);
+				Thread.sleep(1000);
+				iters--;
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		System.out.println("adding new image.");
+		final Img<UnsignedShortType> imgB = ArrayImgs.unsignedShorts(100, 100, 30);
+		imgB.forEach(p -> p.setReal(rng.nextFloat()));
+		session.addToLastOrInNewBdv(imgB,"source 2");
+
+		iters = 20;
+		while (iters > 0 && session.isBdvAlive()) {
+			try {
+				System.out.println("Seconds before finishing the program: "+iters);
+				Thread.sleep(1000);
+				iters--;
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		System.out.println("done.");
+	}
+}
