e9d4793ca4b66f20a936b1c4ee1814fdcb972883
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2025-11-17 15:29:03 +0100

    Reformat files with imglib2 style

--- src/main/java/sc/fiji/ome/zarr/plugins/PresetScriptPlugin.java
+++ src/main/java/sc/fiji/ome/zarr/plugins/PresetScriptPlugin.java
@@ -1,28 +1,29 @@
 package sc.fiji.ome.zarr.plugins;
 
-import org.scijava.plugin.Parameter;
-import org.scijava.plugin.Plugin;
 import org.scijava.command.Command;
 import org.scijava.log.LogService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
 import org.scijava.widget.FileWidget;
+
 import java.io.File;
 
-@Plugin(type = Command.class, menuPath = "Plugins>NGFF OME-Zarr>Preset DragAndDrop User Script",
-		name = "DnDUserScript", headless = true)
+@Plugin( type = Command.class, menuPath = "Plugins>NGFF OME-Zarr>Preset DragAndDrop User Script", name = "DnDUserScript", headless = true )
 public class PresetScriptPlugin implements Command
 {
 
-	@Parameter(label = "Tooltip text:")
+	@Parameter( label = "Tooltip text:" )
 	String line;
 
-	@Parameter(label = "Script to be executed:", style = FileWidget.OPEN_STYLE)
+	@Parameter( label = "Script to be executed:", style = FileWidget.OPEN_STYLE )
 	File scriptPath;
 
 	@Parameter
 	LogService log;
 
 	@Override
-	public void run() {
-		log.info("Thanks, memorizing it now...");
+	public void run()
+	{
+		log.info( "Thanks, memorizing it now..." );
 	}
 }
