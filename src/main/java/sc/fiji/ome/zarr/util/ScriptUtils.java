91de68387e7ce8567aefb16860b5f43b4e7c48d5
Vladimír Ulman <ulman@fi.muni.cz>
2026-01-15 01:04:42 +0100

    ScriptUtils code tiny simplified, dropped (explicit) need of ModuleService

--- src/main/java/sc/fiji/ome/zarr/util/ScriptUtils.java
+++ src/main/java/sc/fiji/ome/zarr/util/ScriptUtils.java
@@ -1,9 +1,8 @@
 package sc.fiji.ome.zarr.util;
 
 import org.scijava.Context;
-import org.scijava.module.Module;
-import org.scijava.module.ModuleService;
 import org.scijava.prefs.PrefService;
+import org.scijava.script.ScriptModule;
 import org.scijava.script.ScriptService;
 import org.scijava.ui.swing.script.TextEditor;
 import org.slf4j.Logger;
@@ -33,10 +32,11 @@ public class ScriptUtils
 	public static void executePresetScript( final Context ctx, final String inputPath )
 	{
 		ScriptService scriptService = ctx.getService( ScriptService.class );
-		ModuleService moduleService = ctx.getService( ModuleService.class );
 		PrefService prefService = ctx.getService( PrefService.class );
-		if ( scriptService == null || moduleService == null || prefService == null )
+		if ( scriptService == null || prefService == null ) {
+			logger.error( "Failed obtaining Script and/or Pref services. Is Fiji properly initiated?" );
 			return;
+		}
 
 		//retrieve the path to the preset script
 		final String scriptPath = prefService.get( PresetScriptPlugin.class, "scriptPath", "--none--" );
@@ -46,9 +46,8 @@ public class ScriptUtils
 			//the filepath is viable, let's run the script
 			try
 			{
-				final Module module = moduleService.createModule( scriptService.getScript( new File( scriptPath ) ) );
+				ScriptModule module = scriptService.getScript(new File(scriptPath)).createModule();
 				module.setInput( "path", inputPath );
-
 				logger.info( "Executing script: {}", scriptPath );
 				logger.info( "on String parameter: {}", inputPath );
 				module.run();
