4bd0bf666cfbd59771a7811b590913a7d8ba40e2
hahmann <hahmann@mpi-cbg.de>
2026-02-13 16:38:59 +0100

    Switch `MultiscaleImageDemo` to use local Zarr file path for testing purposes

--- src/test/java/sc/fiji/ome/zarr/pyramid/MultiscaleImageDemo.java
+++ src/test/java/sc/fiji/ome/zarr/pyramid/MultiscaleImageDemo.java
@@ -5,7 +5,8 @@ import net.imagej.ImageJ;
 public class MultiscaleImageDemo {
 	public static void main( String[] args )
 	{
-		final String multiscalePath = "https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.4/idr0079A/idr0079_images.zarr/0";
+		// final String multiscalePath = "https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.4/idr0079A/idr0079_images.zarr/0";
+		final String multiscalePath = "/Users/hahmann/Data/idr0079_images.zarr/0"; // https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.4/idr0079A/idr0079_images.zarr/0
 
 		final MultiscaleImage< ?, ? > multiscaleImage = new MultiscaleImage<>( multiscalePath );
 		multiscaleImage.dimensions();
