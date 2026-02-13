c23a615d67d0bd996b5ae2241cc98310b7771038
hahmann <hahmann@mpi-cbg.de>
2026-02-13 16:37:56 +0100

    Add code again to enable MultiscaleImageDemo to Multiscales class

--- src/main/java/sc/fiji/ome/zarr/pyramid/Multiscales.java
+++ src/main/java/sc/fiji/ome/zarr/pyramid/Multiscales.java
@@ -3,16 +3,49 @@ package sc.fiji.ome.zarr.pyramid;
 import java.util.Collections;
 import java.util.List;
 
+import com.google.common.collect.Lists;
+import com.google.gson.JsonElement;
+
 /**
  * This is basically one pyramid, which is a list of
  * incrementally down-scaled images (referred here as 'scales').
  */
 public class Multiscales
 {
+	// key in json for multiscales
+	public static final String MULTI_SCALE_KEY = "multiscales";
+
     private final String name;
     private final List< Axis > axes;    //length matches number of dimensions
     private final List< Scale > scales; //length matches the number of resolution levels
     private final int numDimensions;
+
+	// Simply contains the {@codeAxes[] axes}
+	// but in reversed order to accommodate
+	// the Java array ordering of the image data.
+	private List< Axis > axisList;
+
+	private Dataset[] datasets;
+
+	private CoordinateTransformations[] coordinateTransformations; // from v0.4+ within JSON
+
+	public static class Dataset
+	{
+		public String path;
+
+		public CoordinateTransformations[] coordinateTransformations;
+	}
+
+	public static class CoordinateTransformations
+	{
+		public String type;
+
+		public double[] scale;
+
+		public double[] translation;
+
+		public String path;
+	}
 
     public Multiscales(String name, List< Axis > axes, List< Scale > scales) {
         this.name = name;
@@ -20,6 +53,38 @@ public class Multiscales
         this.scales = scales;
         this.numDimensions = axes.size();
     }
+
+	public void init()
+	{
+		axisList = Lists.reverse( axes );
+		// numDimensions = axisList.size();
+	}
+
+	// TODO Can this be done with a JSONAdapter ?
+	public void applyVersionFixes( JsonElement multiscales )
+	{
+		String version = multiscales.getAsJsonObject().get( "version" ).getAsString();
+		if ( version.equals( "0.3" ) )
+		{
+			JsonElement axes = multiscales.getAsJsonObject().get( "axes" );
+			// FIXME
+			//   - populate Axes[]
+			//   - populate coordinateTransformations[]
+			throw new RuntimeException( "Parsing version 0.3 not yet implemented." );
+		}
+		else if ( version.equals( "0.4" ) )
+		{
+			// This should just work automatically
+		}
+		else
+		{
+			JsonElement axes = multiscales.getAsJsonObject().get( "axes" );
+			// FIXME
+			//   - populate Axes[]
+			//   - populate coordinateTransformations[]
+			throw new RuntimeException( "Parsing version " + version + " is not yet implemented." );
+		}
+	}
 
     /**
      * This is a representation of a single scale, typically this
@@ -85,6 +150,16 @@ public class Multiscales
     {
         return Collections.unmodifiableList( scales );
     }
+
+	public CoordinateTransformations[] getCoordinateTransformations()
+	{
+		return coordinateTransformations;
+	}
+
+	public Dataset[] getDatasets()
+	{
+		return datasets;
+	}
 
     public int numDimensions()
     {
