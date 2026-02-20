46557f1e8ec3a5e00f7cd1552a8189dd8a554ee1
hahmann <hahmann@mpi-cbg.de>
2026-02-20 16:29:33 +0100

    Allow instantiation of DefaultPyramidal5DImageData without MultiScaleImage
    - Add Pyramidal5DImageDataDemo

---
+++ src/test/java/sc/fiji/ome/zarr/pyramid/Pyramidal5DImageDataDemo.java
@@ -0,0 +1,23 @@
+package sc.fiji.ome.zarr.pyramid;
+
+import net.imagej.ImageJ;
+
+public class Pyramidal5DImageDataDemo
+{
+	public static void main( String[] args )
+	{
+		// final String multiscalePath = "https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.4/idr0079A/idr0079_images.zarr/0";
+		final String multiscalePath = "/Users/hahmann/Data/idr0079_images.zarr/0"; // https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.4/idr0079A/idr0079_images.zarr/0
+
+		// Show as imagePlus
+		final ImageJ imageJ = new ImageJ();
+		imageJ.ui().showUI();
+		final DefaultPyramidal5DImageData< ?, ? > pyramidal5DImageData =
+				new DefaultPyramidal5DImageData<>( imageJ.context(), multiscalePath );
+		PyramidalDataset< ? > pyramidalDataset = pyramidal5DImageData.asPyramidalDataset();
+		imageJ.ui().show( pyramidalDataset );
+
+		// Also show the displayed image in BDV
+		imageJ.command().run( OpenInBDVCommand.class, true );
+	}
+}
