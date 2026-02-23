1ffa8aad6125367632dfb15e04fb210f4abc47de
hahmann <hahmann@mpi-cbg.de>
2026-02-23 14:54:37 +0100

    Extract N5ReaderDemo from DefaultPyramidal5DImageData and move to test directory

--- src/main/java/sc/fiji/ome/zarr/pyramid/DefaultPyramidal5DImageData.java
+++ src/main/java/sc/fiji/ome/zarr/pyramid/DefaultPyramidal5DImageData.java
@@ -277,31 +277,6 @@ public class DefaultPyramidal5DImageData<
 		ijDataset.setRGBMerged( false );
 	}
 
-	public static void main( String[] args )
-	{
-		//final String path = "/home/ulman/Documents/talks/CEITEC/2025_11_ZarrSymposium_Zurich/data/MitoEM_fixedRes.zarr/MitoEM_fixedRes";
-		final String path = "https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.5/idr0033A/BR00109990_C2.zarr/0";
-		final N5Reader n5reader = new N5Factory().openReader( path );
-		System.out.println( "got reader: " + n5reader );
-		N5Metadata m = N5DatasetDiscoverer.discover( n5reader ).getMetadata();
-		System.out.println( "got metadata: " + m );
-
-		/*
-		if (m instanceof OmeNgffV05Metadata) {
-			OmeNgffV05Metadata mv05 = (OmeNgffV05Metadata)m;
-			System.out.println("name: "+mv05.getName());
-			for (OmeNgffMultiScaleMetadata ms : mv05.multiscales) {
-				System.out.println("----------------");
-				System.out.println("name: "+ms.name+"     type: "+ms.type+"     version: "+ms.version);
-				System.out.println(ms.coordinateTransformations);
-			}
-		}
-		*/
-
-		//TODO fetch v0.5 NGFF Metadata -> ask John
-		//there was supposed to be some class for it
-	}
-
 	@Override
 	public PyramidalDataset< T > asPyramidalDataset()
 	{
