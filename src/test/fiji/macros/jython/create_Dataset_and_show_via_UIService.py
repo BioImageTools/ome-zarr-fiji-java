c38f9cc33aba25fdb0f3cf86de9e07627fe696d1
Vladimír Ulman <ulman@fi.muni.cz>
2026-02-26 23:06:01 +0100

    FIX: create_Dataset demonstrates good use of a Dataset...
    ...when it is displayed in BDV, and yet emulating as if it was
    displayed with the standard IJ1 windows. The trick is in using
    the Dataset#incrementReferences() and decrementReferences().
    
    Indeed, when references count drops to zero, the Dataset is released
    and no longer available anywhere in Fiji.

--- src/test/fiji/macros/jython/create_Dataset_and_show_via_UIService.py
+++ src/test/fiji/macros/jython/create_Dataset_and_show_via_UIService.py
@@ -1,12 +1,13 @@
 #@ DatasetService dsService
-#@ ObjectService objService
 #@ UIService ui
 #@ boolean display_image (label="Display the created image:")
 #@ string image_label (label="Title of the created image:", value="ImgPlus turned into Dataset #1")
 
 from net.imagej import ImgPlus
 from net.imglib2.img.planar import PlanarImgs
+from bdv.util import BdvFunctions
 
+# ------- standard creation of `img` and wrapping it "till it becomes" a `Dataset` -------
 img = PlanarImgs.bytes(303, 302)
 imp = ImgPlus(img, image_label)
 ds = dsService.create(imp)
@@ -15,7 +16,26 @@ print("Created   img  of  id: "+str(id(img)))
 print("Created ImgPlus of id: "+str(id(imp)))
 print("Created dataset of id: "+str(id(ds)))
 
+
+# ------- this is a Jython version of attaching a listener to "onWindowClose" event in BDV -------
+from java.awt.event import WindowAdapter
+
+def register_datasetDecrement_onBdvCloseWindow(bdv_handle, dataset):
+	viewer_panel = bdv_handle.getViewerPanel()
+	win = viewer_panel.getRootPane().getParent()
+
+	class OnClose(WindowAdapter):
+		def windowClosed(self, event):
+			print("BDV window closed, decrementing id: "+str(id(dataset)))
+			dataset.decrementReferences()
+
+	win.addWindowListener(OnClose())
+
+
+# ------- finally show the `Dataset` either way(s) -------
 if display_image:
 	ui.show(ds)
 else:
-	objService.addObject(ds)
+	ds.incrementReferences()
+	bdv_stack_source = BdvFunctions.show(ds.getImgPlus(), image_label)
+	register_datasetDecrement_onBdvCloseWindow(bdv_stack_source.getBdvHandle(), ds)
