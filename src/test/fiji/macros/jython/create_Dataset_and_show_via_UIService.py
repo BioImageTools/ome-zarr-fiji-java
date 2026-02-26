a1711d22a070dea3c31f4c9a7c6bd4800fd71b49
Vladimír Ulman <ulman@fi.muni.cz>
2026-02-26 19:48:43 +0100

    COS: create_Dataset more verbose with fine-grained control

--- src/test/fiji/macros/jython/create_Dataset_and_show_via_UIService.py
+++ src/test/fiji/macros/jython/create_Dataset_and_show_via_UIService.py
@@ -1,12 +1,21 @@
-#@ Context ctx
+#@ DatasetService dsService
+#@ ObjectService objService
 #@ UIService ui
+#@ boolean display_image (label="Display the created image:")
+#@ string image_label (label="Title of the created image:", value="ImgPlus turned into Dataset #1")
 
 from net.imagej import ImgPlus
 from net.imglib2.img.planar import PlanarImgs
-from net.imagej import DefaultDataset
 
 img = PlanarImgs.bytes(303, 302)
-imp = ImgPlus(img, "ImgPlus turned into Dataset")
-ds = DefaultDataset(ctx, imp)
+imp = ImgPlus(img, image_label)
+ds = dsService.create(imp)
 
-ui.show(ds)
+print("Created   img  of  id: "+str(id(img)))
+print("Created ImgPlus of id: "+str(id(imp)))
+print("Created dataset of id: "+str(id(ds)))
+
+if display_image:
+	ui.show(ds)
+else:
+	objService.addObject(ds)
