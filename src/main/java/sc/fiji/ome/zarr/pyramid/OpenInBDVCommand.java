5449001a1527fb5e399cba77b13dee896018b5fe
hahmann <hahmann@mpi-cbg.de>
2026-03-02 12:34:45 +0100

    Change log level 0 (i.e. NONE) to more explicit and better fitting DEBUG

--- src/main/java/sc/fiji/ome/zarr/pyramid/OpenInBDVCommand.java
+++ src/main/java/sc/fiji/ome/zarr/pyramid/OpenInBDVCommand.java
@@ -32,6 +32,7 @@ import net.imagej.Dataset;
 import net.imglib2.util.Cast;
 
 import org.scijava.command.Command;
+import org.scijava.log.LogLevel;
 import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
@@ -51,9 +52,9 @@ public class OpenInBDVCommand implements Command
 	@Override
 	public void run()
 	{
-		logService.log( 0, "Trying to open: " + dataset.getName() );
-		logService.log( 0, "Class: " + dataset.getClass() );
-		logService.log( 0, "Dataset instanceof PyramidalDataset: " + ( dataset instanceof PyramidalDataset ) );
+		logService.log( LogLevel.DEBUG, "Trying to open: " + dataset.getName() );
+		logService.log( LogLevel.DEBUG, "Class: " + dataset.getClass() );
+		logService.log( LogLevel.DEBUG, "Dataset instanceof PyramidalDataset: " + ( dataset instanceof PyramidalDataset ) );
 		if ( dataset instanceof PyramidalDataset )
 		{
 			logService.log( 0, "Opening " + dataset.getClass() + " in BDV..." );
