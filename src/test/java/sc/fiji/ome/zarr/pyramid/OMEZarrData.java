32a4004e4734be1912cc052791b5b6e182c1de84
hahmann <hahmann@mpi-cbg.de>
2026-04-02 13:51:09 +0200

    Add copyright headers for OME-Zarr Fiji code files.

--- src/test/java/sc/fiji/ome/zarr/pyramid/OMEZarrData.java
+++ src/test/java/sc/fiji/ome/zarr/pyramid/OMEZarrData.java
@@ -1,18 +1,18 @@
 /*-
  * #%L
- * Expose the Imaris XT interface as an ImageJ2 service backed by ImgLib2.
+ * OME-Zarr extras for Fiji
  * %%
- * Copyright (C) 2019 - 2021 Bitplane AG
+ * Copyright (C) 2022 - 2026 SciJava developers
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
+ * 
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
