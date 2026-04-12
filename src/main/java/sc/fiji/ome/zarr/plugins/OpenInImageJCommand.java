654dede6ad78d0cd7ef0a35d3bb3467d7ffcf6c8
Vladimír Ulman <ulman@fi.muni.cz>
2026-04-13 00:32:29 +0200

    ADD: ImagePlusFromZarr plugin

--- src/main/java/sc/fiji/ome/zarr/plugins/OpenInImageJCommand.java
+++ src/main/java/sc/fiji/ome/zarr/plugins/OpenInImageJCommand.java
@@ -1,24 +1,12 @@
 package sc.fiji.ome.zarr.plugins;
 
-import bdv.viewer.SourceAndConverter;
-import edu.mines.jtk.dsp.Real1Test;
-import ij.ImagePlus;
-import ij.process.FloatProcessor;
 import net.imagej.Dataset;
 import net.imagej.DatasetService;
-import net.imagej.ImgPlus;
-import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.img.Img;
-import net.imglib2.img.display.imagej.ImageJFunctions;
-import net.imglib2.type.NativeType;
-import net.imglib2.type.numeric.RealType;
-import net.imglib2.view.Views;
 import org.scijava.command.Command;
 import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.scijava.ui.UIService;
-import sc.fiji.ome.zarr.pyramid.PyramidalDataset;
 
 @Plugin( type = Command.class, menuPath = "Plugins > OME-Zarr > Open Current OME-Zarr Image in ImageJ" )
 public class OpenInImageJCommand implements Command
@@ -58,19 +46,5 @@ public class OpenInImageJCommand implements Command
 				+ ", depth=" + z + ", channels=" + c + ", time points=" + t );
 
 		uiService.show( d );
-
-	}
-
-	public static < T extends NativeType< T > & RealType< T > > ImagePlus getImagePlus(
-			Dataset d, int resolutionLevel,
-			int channel, int timepoint )
-	{
-		if ( !( d instanceof PyramidalDataset ) )
-			return null;
-
-		PyramidalDataset< T > pd = ( PyramidalDataset< T > ) d;
-		SourceAndConverter< T > sac = pd.asSources().get( channel );
-		RandomAccessibleInterval< T > rai = sac.getSpimSource().getSource( timepoint, resolutionLevel );
-		return ImageJFunctions.wrap( rai, sac.getSpimSource().getName() );
 	}
 }
---
+++ src/main/java/sc/fiji/ome/zarr/plugins/macros/ImagePlusFromZarr.java
@@ -0,0 +1,62 @@
+package sc.fiji.ome.zarr.plugins.macros;
+
+import bdv.viewer.SourceAndConverter;
+import ij.ImagePlus;
+import net.imagej.Dataset;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
+import org.scijava.ItemIO;
+import org.scijava.command.Command;
+import org.scijava.log.LogService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import sc.fiji.ome.zarr.pyramid.PyramidalDataset;
+
+@Plugin( type = Command.class, menuPath = "Plugins > OME-Zarr > Open Current OME-Zarr Image in ImageJ" )
+public class ImagePlusFromZarr implements Command
+{
+	@Parameter
+	LogService logService;
+
+	@Parameter
+	Dataset d;
+
+	@Parameter
+	int resolutionLevel = 0;
+
+	@Parameter
+	int channel = 0;
+
+	@Parameter
+	int timepoint = 0;
+
+	@Parameter( type = ItemIO.OUTPUT )
+	ImagePlus imagePlus;
+
+	@Override
+	public void run()
+	{
+		imagePlus = getImagePlus( d, resolutionLevel, channel, timepoint );
+
+		if ( imagePlus == null )
+		{
+			logService.error( "The current dataset " + d.getName() + " is not OME-Zarr." );
+			return;
+		}
+	}
+
+	public static < T extends NativeType< T > & RealType< T > > ImagePlus getImagePlus(
+			Dataset d, int resolutionLevel,
+			int channel, int timepoint )
+	{
+		if ( !( d instanceof PyramidalDataset ) )
+			return null;
+
+		PyramidalDataset< T > pd = ( PyramidalDataset< T > ) d;
+		SourceAndConverter< T > sac = pd.asSources().get( channel );
+		RandomAccessibleInterval< T > rai = sac.getSpimSource().getSource( timepoint, resolutionLevel );
+		return ImageJFunctions.wrap( rai, sac.getSpimSource().getName() );
+	}
+}
