174af93281d2354321800301746f125940c6c8e9
hahmann <hahmann@mpi-cbg.de>
2026-04-16 15:04:51 +0200

    Add check for script execution failures in `ZarrOpenActions` unit tests

--- src/test/java/sc/fiji/ome/zarr/util/ZarrOpenActionsTest.java
+++ src/test/java/sc/fiji/ome/zarr/util/ZarrOpenActionsTest.java
@@ -62,7 +62,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -507,7 +509,12 @@ class ZarrOpenActionsTest
 			prefService.put( DragAndDropUserScriptSettings.class, "scriptPath", tempFile.getAbsolutePath() );
 			String resource = "sc/fiji/ome/zarr/util/5d_testing/5d_dataset_v5.ome.zarr";
 			Path path = ZarrTestUtils.resourcePath( resource );
-			ZarrOpenActions actions = new ZarrOpenActions( path, context, null, System.out::println );
+			AtomicBoolean scriptFailed = new AtomicBoolean( false );
+			Consumer< String > errorHandler = errorMessage -> {
+				scriptFailed.set( true );
+				System.out.println( errorMessage );
+			};
+			ZarrOpenActions actions = new ZarrOpenActions( path, context, null, errorHandler );
 			actions.runScript();
 
 			boolean foundTextEditor = false;
@@ -530,6 +537,7 @@ class ZarrOpenActionsTest
 			}
 			assertFalse( foundTextEditor, "TextEditor window should not be open" );
 			assertTrue( foundBigDataViewer, "BigDataViewer window should be open" );
+			assertFalse( scriptFailed.get(), "Script should not have failed" );
 
 			for ( Window window : Window.getWindows() )
 				window.dispose();
