0827e0a895eeb1a2403e754b7204fad0dc0729d8
hahmann <hahmann@mpi-cbg.de>
2026-04-27 14:41:02 +0200

    Update `DragAndDropBehaviorSettings` to reflect OME-Zarr support versions

--- src/main/java/sc/fiji/ome/zarr/plugins/DragAndDropBehaviorSettings.java
+++ src/main/java/sc/fiji/ome/zarr/plugins/DragAndDropBehaviorSettings.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -95,8 +95,8 @@ public class DragAndDropBehaviorSettings extends DynamicCommand
 	@Parameter( visibility = ItemVisibility.MESSAGE, required = false, persist = false )
 	private String readerBackendInfo = "<html>"
 			+ "<body width=" + WIDTH + "cm align=left>"
-			+ "N5 universe supports Zarr v2 (via n5-zarr).<br>"
-			+ "zarr-java supports both Zarr v2 and v3."
+			+ "N5 supports OME-Zarr v0.4 (Zarr v2) - v0.5 (Zarr v3).<br>"
+			+ "zarr-java supports OME-Zarr v0.4 (Zarr v2) - v0.3 (Zarr v3)."
 			+ "</body>"
 			+ "</html>";
 
@@ -131,7 +131,8 @@ public class DragAndDropBehaviorSettings extends DynamicCommand
 	@SuppressWarnings( "unused" )
 	private void initZarrReaderBackends()
 	{
-		getInfo().getMutableInput( "readerBackendChoice", String.class ).setChoices( readerBackendDescriptions( ZarrReaderBackend.values() ) );
+		getInfo().getMutableInput( "readerBackendChoice", String.class )
+				.setChoices( readerBackendDescriptions( ZarrReaderBackend.values() ) );
 	}
 
 	static List< String > enumNamesAsList( final ZarrOpenBehavior[] values )
