6309a8e913276eed28b4713402ec25f24e42afc3
hahmann <hahmann@mpi-cbg.de>
2026-05-06 13:34:17 +0200

    Reorganize DnDActionChooser panel layout: 3x2 instead of 2x3

--- src/main/java/sc/fiji/ome/zarr/ui/DnDActionChooser.java
+++ src/main/java/sc/fiji/ome/zarr/ui/DnDActionChooser.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -142,12 +142,12 @@ public class DnDActionChooser
 		JPanel panel;
 		if ( extendedVersion )
 		{
-			panel = new JPanel( new GridLayout( 3, 2, 5, 5 ) );
+			panel = new JPanel( new GridLayout( 2, 3, 5, 5 ) );
 			panel.add( zarrToIJDialog );
-			panel.add( zarrToBDVDialog );
 			panel.add( zarrIJHighestResolution );
-			panel.add( zarrBDVHighestResolution );
 			panel.add( zarrScript );
+			panel.add( zarrToBDVDialog );
+			panel.add( zarrBDVHighestResolution );
 			panel.add( help );
 		}
 		else
