032124bfe9f82e18c0b0ebf39d632c673274f745
hahmann <hahmann@mpi-cbg.de>
2026-05-12 13:52:49 +0200

    Improve exception message of NotAMultiscaleImageException

--- src/main/java/sc/fiji/ome/zarr/pyramid/exceptions/NotAMultiscaleImageException.java
+++ src/main/java/sc/fiji/ome/zarr/pyramid/exceptions/NotAMultiscaleImageException.java
@@ -33,11 +33,11 @@ public class NotAMultiscaleImageException extends RuntimeException
 
 	public NotAMultiscaleImageException( final String path )
 	{
-		super( "The dataset at path '" + path + "' is not a valid OME-Zarr multiscale image." );
+		super( "The dataset at path " + path + " is not a valid OME-Zarr multiscale image." );
 	}
 
 	public NotAMultiscaleImageException( final String path, final Throwable cause )
 	{
-		super( "The dataset at path '" + path + "' is not a valid OME-Zarr multiscale image. Cause: " + cause.getMessage(), cause );
+		super( "The dataset at path " + path + " is not a valid OME-Zarr multiscale image. Cause: " + cause.getMessage(), cause );
 	}
 }
