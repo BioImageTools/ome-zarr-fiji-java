547a3d78b611742b936908d0d5f942f9d277b6eb
hahmann <hahmann@mpi-cbg.de>
2026-05-13 12:22:03 +0200

    Add `@SuppressWarnings("all")` to legacy classes so that they do not show up in sonar cloud

--- src/test/java/sc/fiji/ome/zarr/examples/BigStitcherStyleOMEZarrReader.java
+++ src/test/java/sc/fiji/ome/zarr/examples/BigStitcherStyleOMEZarrReader.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -58,6 +58,7 @@ import software.amazon.awssdk.services.s3.S3Client;
  * BigStitcher/BigDataViewer-style OME-Zarr reader using n5-zarr.
  * This follows the pattern used by bigdataviewer-omezarr and mobie-io projects.
  */
+@SuppressWarnings( "all" )
 public class BigStitcherStyleOMEZarrReader
 {
 
