9130110d824e05385ee07e3d221a791b6c5f5d6d
hahmann <hahmann@mpi-cbg.de>
2026-06-02 18:04:15 +0200

    Improve javadoc (fresh vs. newly created)

--- src/main/java/sc/fiji/ome/zarr/pyramid/Pyramidal5DImageData.java
+++ src/main/java/sc/fiji/ome/zarr/pyramid/Pyramidal5DImageData.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -66,7 +66,7 @@ public interface Pyramidal5DImageData< T extends NativeType< T > & RealType< T >
 
 	/**
 	 * @param resolutionLevel 0-based index into the resolution pyramid (0 = highest resolution)
-	 * @return a fresh {@link PyramidalDataset} wrapping the image at the specified resolution level,
+	 * @return a newly created object of {@link PyramidalDataset} wrapping the image at the specified resolution level,
 	 *   backed by the same underlying pyramid data
 	 */
 	PyramidalDataset< T > asPyramidalDataset( int resolutionLevel );
@@ -80,7 +80,7 @@ public interface Pyramidal5DImageData< T extends NativeType< T > & RealType< T >
 
 	/**
 	 * @param resolutionLevel 0-based index into the resolution pyramid (0 = highest resolution)
-	 * @return a fresh IJ2 {@code net.imagej.Dataset} wrapping the image at the specified resolution level
+	 * @return a newly created IJ2 {@code net.imagej.Dataset} wrapping the image at the specified resolution level
 	 */
 	Dataset asDataset( int resolutionLevel );
 
