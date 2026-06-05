2135807766be6105c099496192eed425329a20fb
tpietzsch <tobias.pietzsch@gmail.com>
2026-06-05 12:52:14 +0200

    WIP: revise tests

--- src/test/java/sc/fiji/ome/zarr/open/ZarrOpenActionsTest.java
+++ src/test/java/sc/fiji/ome/zarr/open/ZarrOpenActionsTest.java
@@ -93,6 +93,7 @@ import ij.ImagePlus;
 import sc.fiji.ome.zarr.plugins.settings.UserScriptSettings;
 import sc.fiji.ome.zarr.pyramid.Pyramidal;
 import sc.fiji.ome.zarr.pyramid.Pyramidal5DImageData;
+import sc.fiji.ome.zarr.pyramid.PyramidalBdvDataset;
 import sc.fiji.ome.zarr.pyramid.PyramidalDataset;
 import sc.fiji.ome.zarr.open.options.ZarrOpeningSettings;
 import sc.fiji.ome.zarr.open.options.ZarrOpenBehavior;
@@ -573,18 +574,11 @@ class ZarrOpenActionsTest
 			ZarrOpenActions actions = new ZarrOpenActions( path.toUri(), context );
 			BdvHandle bdvHandle = Cast.unchecked( actions.openBDVWithImage() );
 
-//			DatasetService datasetService = context.getService( DatasetService.class );
-//			assertNotNull( datasetService );
-//			List< Dataset > datasets = datasetService.getDatasets();
-//			assertNotNull( datasets );
-//			assertEquals( 1, datasets.size() ); // The dataset service knows the dataset now
-
 			BdvFocusService pyramidalService = context.getService( BdvFocusService.class );
 			assertNotNull( pyramidalService );
 			List< Pyramidal > pyramidals = pyramidalService.getPyramidals();
 			assertNotNull( pyramidals );
 			assertEquals( 1, pyramidals.size() ); // The dataset service knows the dataset now
-
 
 			if ( resource.contains( "5d_testing" ) )
 			{
@@ -737,21 +731,25 @@ class ZarrOpenActionsTest
 			DatasetService datasetService = context.getService( DatasetService.class );
 			assertEquals( 2, datasetService.getDatasets().size() );
 
+			BdvFocusService pyramidalService = context.getService( BdvFocusService.class );
+			assertEquals( 2, pyramidalService.getPyramidals().size() );
+
 			// Each BDV open creates one additional dataset backed by the full pyramid
 			BdvHandle bdvHandle1 = null;
 			BdvHandle bdvHandle2 = null;
 			try
 			{
 				bdvHandle1 = Cast.unchecked( actions.openBDVWithImage() );
-				assertEquals( 3, datasetService.getDatasets().size() );
+				assertEquals( 3, pyramidalService.getPyramidals().size() );
 
 				bdvHandle2 = Cast.unchecked( actions.openBDVWithImage() );
-				assertEquals( 4, datasetService.getDatasets().size() );
+				assertEquals( 4, pyramidalService.getPyramidals().size() );
 
 				PyramidalDataset ijLevel0 = Cast.unchecked( datasetService.getDatasets().get( 0 ) );
 				PyramidalDataset ijLevel1 = Cast.unchecked( datasetService.getDatasets().get( 1 ) );
-				PyramidalDataset bdv1 = Cast.unchecked( datasetService.getDatasets().get( 2 ) );
-				PyramidalDataset bdv2 = Cast.unchecked( datasetService.getDatasets().get( 3 ) );
+
+				PyramidalBdvDataset<?> bdv1 = Cast.unchecked( pyramidalService.getPyramidals().get( 2 ) );
+				PyramidalBdvDataset<?> bdv2 = Cast.unchecked( pyramidalService.getPyramidals().get( 3 ) );
 
 				// All 4 datasets (2 IJ + 2 BDV) must be backed by the exact same Pyramidal5DImageData object
 				Pyramidal5DImageData< ? > sharedPyramid = ijLevel0.getPyramidal5DImageData();
@@ -766,10 +764,11 @@ class ZarrOpenActionsTest
 						"IJ dataset at level 0 should have the highest resolution dimensions" );
 				assertArrayEquals( new long[] { 32, 32, 8, 3, 4 }, ijLevel1.getImgPlus().dimensionsAsLongArray(),
 						"IJ dataset at level 1 should have half the spatial dimensions of level 0" );
-				assertArrayEquals( new long[] { 64, 64, 16, 3, 4 }, bdv1.getImgPlus().dimensionsAsLongArray(),
-						"First BDV dataset should reflect level 0 (highest resolution)" );
-				assertArrayEquals( new long[] { 64, 64, 16, 3, 4 }, bdv2.getImgPlus().dimensionsAsLongArray(),
-						"Second BDV dataset should reflect level 0 (highest resolution)" );
+				// TODO: not sure what to test here. PyramidalBdvDataset no longer implements Dataset.
+//				assertArrayEquals( new long[] { 64, 64, 16, 3, 4 }, bdv1.asSources().get( 0 ),
+//						"First BDV dataset should reflect level 0 (highest resolution)" );
+//				assertArrayEquals( new long[] { 64, 64, 16, 3, 4 }, bdv2.getImgPlus().dimensionsAsLongArray(),
+//						"Second BDV dataset should reflect level 0 (highest resolution)" );
 			}
 			finally
 			{
@@ -805,20 +804,21 @@ class ZarrOpenActionsTest
 				// BDV open covers all resolution levels and registers one dataset
 				bdvHandle = Cast.unchecked( actions.openBDVWithImage() );
 
-				DatasetService datasetService = context.getService( DatasetService.class );
-				assertEquals( 1, datasetService.getDatasets().size() );
+				BdvFocusService pyramidalService = context.getService( BdvFocusService.class );
+				assertEquals( 1, pyramidalService.getPyramidals().size() );
 
 				// Opening a specific resolution level in IJ creates a 2nd dataset window,
 				// backed by the same Pyramidal5DImageData as the BDV dataset
 				actions.openIJWithImage( 1 ); // coarser resolution: [x=32, y=32, z=8, c=3, t=4]
-				assertEquals( 2, datasetService.getDatasets().size() );
+				assertEquals( 2, pyramidalService.getPyramidals().size() );
 
-				PyramidalDataset bdvDataset = Cast.unchecked( datasetService.getDatasets().get( 0 ) );
-				PyramidalDataset ijDataset = Cast.unchecked( datasetService.getDatasets().get( 1 ) );
+				PyramidalBdvDataset<?> bdvDataset = Cast.unchecked( pyramidalService.getPyramidals().get( 0 ) );
+				PyramidalDataset ijDataset = Cast.unchecked( pyramidalService.getPyramidals().get( 1 ) );
 				assertSame( bdvDataset.getPyramidal5DImageData(), ijDataset.getPyramidal5DImageData(),
 						"BDV and IJ datasets must share the same Pyramidal5DImageData instance" );
-				assertArrayEquals( new long[] { 64, 64, 16, 3, 4 }, bdvDataset.getImgPlus().dimensionsAsLongArray(),
-						"BDV dataset should reflect level 0 (highest resolution)" );
+				// TODO: not sure what to test here. PyramidalBdvDataset no longer implements Dataset.
+//				assertArrayEquals( new long[] { 64, 64, 16, 3, 4 }, bdvDataset.getImgPlus().dimensionsAsLongArray(),
+//						"BDV dataset should reflect level 0 (highest resolution)" );
 				assertArrayEquals( new long[] { 32, 32, 8, 3, 4 }, ijDataset.getImgPlus().dimensionsAsLongArray(),
 						"IJ dataset at level 1 should have half the spatial dimensions of level 0" );
 			}
