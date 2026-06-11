05894c8273bf8db686ebb338e9b69dd0bec22bdc
hahmann <hahmann@mpi-cbg.de>
2026-06-11 14:01:23 +0200

    List pyramidal datasets alongside known datasets in Jython test script.

--- src/test/fiji/macros/jython/list_known_datasets.py
+++
@@ -1,7 +0,0 @@
-#@ DatasetService ds
-
-print("----")
-for dimg in ds.getDatasets():
-	print("image '"+dimg.getImgPlus().getName()+"' (id: "+str(id(dimg))+\
-	") of width x height: "+str(dimg.getWidth())+'x'+str(dimg.getHeight()))
-print("----")
