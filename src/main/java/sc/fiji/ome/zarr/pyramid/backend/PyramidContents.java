ccebecf749236364ee5c1efa56d7c8982f90cf1c
hahmann <hahmann@mpi-cbg.de>
2026-06-11 09:29:44 +0200

    Improve Javadoc in `PyramidContents`

--- src/main/java/sc/fiji/ome/zarr/pyramid/backend/PyramidContents.java
+++ src/main/java/sc/fiji/ome/zarr/pyramid/backend/PyramidContents.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -45,7 +45,7 @@ import sc.fiji.ome.zarr.pyramid.metadata.Omero;
  * image data class copies these fields into its own state.
  * <p>
  * Indices in {@code cachedCellImgs}, {@code volatileImgs} and {@code transforms}
- * are in resolution-level order (index 0 is highest resolution).
+ * are in resolution-level order (index 0 is the highest resolution).
  * The imglib2 axis indices follow F-order (x, y, z, c, t), the order produced
  * after the backend has reversed any zarr C-order shapes.
  *
