9bff2cdea933854fdef605b116b4177d13ad1363
hahmann <hahmann@mpi-cbg.de>
2026-08-11 12:05:13 +0200

    URIs naming a host are rejcted with a message by stringToUri()

--- ome-zarr-fiji-ui/src/main/java/ome/zarr/fijiui/util/ClipboardUtils.java
+++ ome-zarr-fiji-ui/src/main/java/ome/zarr/fijiui/util/ClipboardUtils.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -146,6 +146,12 @@ public final class ClipboardUtils
 		if ( parsed != null )
 		{
 			final String scheme = parsed.getScheme();
+			if ( "file".equalsIgnoreCase( scheme ) && parsed.getAuthority() != null )
+			{
+				errorHandler.accept( "A 'file:' URL cannot name a host:\n" + text + "\n\n"
+						+ "Use file:///path/to/data.zarr (three slashes) for a local path." );
+				return null;
+			}
 			if ( "http".equalsIgnoreCase( scheme ) || "https".equalsIgnoreCase( scheme )
 					|| "file".equalsIgnoreCase( scheme ) || "s3".equalsIgnoreCase( scheme ) )
 				return parsed;
