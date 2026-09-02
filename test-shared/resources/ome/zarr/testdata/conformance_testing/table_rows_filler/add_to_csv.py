a6d168eb8659075c6cbdd8e0fd0d740c2665df19
Vladimír Ulman <ulman@fi.muni.cz>
2026-09-02 20:45:45 +0200

    ADD: reproduce_table_v1() to (attempt to) reproduce how v1 table was created

--- test-shared/resources/ome/zarr/testdata/conformance_testing/table_rows_filler/add_to_csv.py
+++ test-shared/resources/ome/zarr/testdata/conformance_testing/table_rows_filler/add_to_csv.py
@@ -25,3 +25,38 @@ def convert_from_v0_table():
     return table
 
 
+def reproduce_table_v1():
+    import pandas as pd
+    import add_to_csv
+
+    t = add_to_csv.convert_from_v0_table()
+    t2a = add_to_csv.convert_from_table(t)
+
+    # bf2raw "failers" -- have to be used with explicit 'PathToImageMultiscales':
+    q = V._create_expected('https://livingobjects.ebi.ac.uk/idr/zarr/v0.4/idr0048A/9846151.zarr','/0')
+    q['StudyName']='idr0048A'
+    q['License']='CC BY 4.0'
+    t2a = pd.concat([t2a, pd.DataFrame([q])])
+
+    q = V._create_expected('https://livingobjects.ebi.ac.uk/idr/zarr/v0.5/idr0026/3.66.9-6.141020_15-41-29.00.ome.zarr','/0')
+    q['StudyName']='idr0026'
+    q['License']='CC BY 4.0'
+    t2a = pd.concat([t2a, pd.DataFrame([q])])
+
+    q = V._create_expected('https://livingobjects.ebi.ac.uk/idr/zarr/v0.5/idr0051/180712_H2B_22ss_Courtney1_20180712-163837_p00_c00_preview.zarr','/0')
+    q['StudyName']='idr0051'
+    q['License']='CC BY 4.0'
+    t2a = pd.concat([t2a, pd.DataFrame([q])])
+
+    for i in range(9):
+        q = V._create_expected('https://livingobjects.ebi.ac.uk/idr/zarr/v0.5/idr0033A/BR00109990_C2.zarr',f"/{i}")
+        q['StudyName']=f"idr0033A_{i}"
+        q['License']='CC BY 4.0'
+        t2a = pd.concat([t2a, pd.DataFrame([q])])
+
+    for i in range(3):
+        q = V._create_expected('https://livingobjects.ebi.ac.uk/idr/zarr/v0.4/idr0079A/idr0079_images.zarr',f"/{i}")
+        q['StudyName']=f"idr0079A_{i}"
+        q['License']='CC BY 4.0'
+        t2a = pd.concat([t2a, pd.DataFrame([q])])
+
