1c90db9afb6c4cb64ec1f8527f06ecc89dcdac18
Vladimír Ulman <ulman@fi.muni.cz>
2026-09-02 16:18:55 +0200

    ADD: add_to_csv.py -- generator of a benchmark table using the "table_rows_filler"

---
+++ test-shared/resources/ome/zarr/testdata/conformance_testing/table_rows_filler/add_to_csv.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import verify_ome_zarr as V
+
+def convert_from_table(old_table):
+    new_table = pd.DataFrame([])
+    for _, row in old_table.iterrows():
+        print("Inspecting path:", row['File Path'])
+        new_content = V._create_expected(row['File Path'],'')
+        new_content['StudyName'] = row['Study']
+        new_content['License'] = row['License']
+        new_table = pd.concat([new_table, pd.DataFrame([new_content])], ignore_index=True)
+    return new_table
+
+
+def convert_from_v1_table():
+    old_table = pd.read_csv('../testbed_datasets.csv')
+    table = old_table[  (old_table['OME-NGFF version'] >= 0.4)
+                      & (old_table['OME-NGFF version'] <= 0.5)                        # version restriction
+                      & (old_table['File Path'].str.contains("livingobjects.ebi"))    # source site restriction
+                      & (old_table['Wells'].isna()) ]                                 # non-HCS sources
+    return table
+
+
