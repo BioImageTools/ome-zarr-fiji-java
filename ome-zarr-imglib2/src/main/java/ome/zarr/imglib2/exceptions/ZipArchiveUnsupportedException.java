cae66ad4daff51278083f94f5929ea52d2587f5a
hahmann <hahmann@mpi-cbg.de>
2026-09-10 10:04:24 +0200

    Shorten exception comments in `ZipArchiveUnsupportedException`.

--- ome-zarr-imglib2/src/main/java/ome/zarr/imglib2/exceptions/ZipArchiveUnsupportedException.java
+++ ome-zarr-imglib2/src/main/java/ome/zarr/imglib2/exceptions/ZipArchiveUnsupportedException.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -30,7 +30,7 @@ package ome.zarr.imglib2.exceptions;
 
 /**
  * Thrown when a zipped OME-Zarr archive ({@code .ozx}) is opened with a backend
- * that cannot read one; zarr-java is the only backend that can.
+ * that cannot read one.
  * <p>
  * This is a property of the selected reader, not of the data, so it is raised
  * before the store is touched. Catch it ahead of {@link StoreAccessException}
