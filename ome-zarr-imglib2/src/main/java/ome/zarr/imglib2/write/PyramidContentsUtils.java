98bb694a6a099adf1439beeb4bb47e0e85c5a6c2
Vladimír Ulman <ulman@fi.muni.cz>
2026-09-21 22:03:58 +0200

    TMP: PyramidContentsUtils.main() to be REMOVED, now for demo/testing

--- ome-zarr-imglib2/src/main/java/ome/zarr/imglib2/write/PyramidContentsUtils.java
+++ ome-zarr-imglib2/src/main/java/ome/zarr/imglib2/write/PyramidContentsUtils.java
@@ -9,6 +9,7 @@ import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.view.Views;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
 import ome.zarr.imglib2.PyramidContents;
 import ome.zarr.imglib2.metadata.AxisCalibration;
 import ome.zarr.imglib2.metadata.Omero;
@@ -330,5 +331,40 @@ public class PyramidContentsUtils
 			view = Views.hyperSlice( view, permutation[ axisIndexTime ], timepoint );
 		}
 		return view;
+	}
+
+
+	public static void main( String[] args )
+	{
+		AxisCalibration[] axes = new AxisCalibration[] {
+				new AxisCalibration( AxisCalibration.T, "seconds", 1 ),
+				new AxisCalibration( AxisCalibration.Y, "microns", 1.6 ),
+				new AxisCalibration( AxisCalibration.X, "microns", 1.2 ),
+		};
+
+		PyramidContents< UnsignedShortType > p = PyramidContentsUtils.create(
+				"empty pyramidal",
+				new UnsignedShortType(),
+				new long[] { 512, 1024 },
+				0,
+				5,
+				axes,
+				new double[][] {
+						{ 2.0, 4.1 },
+						{ 10.0, 1.0 },
+						{ 2.2, 4.3 }
+				}
+		);
+
+		System.out.println( p );
+
+		CachedCellImg< UnsignedShortType, ? > ccimg = ( CachedCellImg< UnsignedShortType, ? > ) p.asLargestImg();
+		System.out.print( "grid sizes: " );
+		for ( int d : ccimg.getCellGrid().getCellDimensions() )
+			System.out.print( d + " px, " );
+		System.out.println();
+
+		RandomAccessibleInterval< UnsignedShortType > newImg = xyzReducedView( p, 0, 3, 3 );
+		System.out.println( newImg );
 	}
 }
