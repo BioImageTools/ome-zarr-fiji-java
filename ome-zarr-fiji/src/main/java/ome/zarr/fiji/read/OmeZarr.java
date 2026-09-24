729d6d11b7d683b3fb9a6257d4fe9ccb088330ba
hahmann <hahmann@mpi-cbg.de>
2026-09-24 17:06:10 +0200

    Modify debug logging for PyramidContents.

--- ome-zarr-fiji/src/main/java/ome/zarr/fiji/read/OmeZarr.java
+++ ome-zarr-fiji/src/main/java/ome/zarr/fiji/read/OmeZarr.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -231,19 +231,7 @@ public class OmeZarr
 	{
 		if ( !logger.isDebugEnabled() )
 			return;
-		logger.debug( "Opened image {} with dimensions x={}, y={}, z={}, c={}, t={}, resolution levels={}",
-				inputUri,
-				axisSize( contents, AxisCalibration.X ),
-				axisSize( contents, AxisCalibration.Y ),
-				axisSize( contents, AxisCalibration.Z ),
-				axisSize( contents, AxisCalibration.C ),
-				axisSize( contents, AxisCalibration.T ),
-				contents.numResolutionLevels() );
-	}
-
-	private static String axisSize( final PyramidContents< ? > contents, final String axisName )
-	{
-		return contents.hasAxis( axisName ) ? Long.toString( contents.sizeAlongAxis( axisName ) ) : "absent";
+		logger.debug( "Pyramid contents of {}:\n{}", inputUri, contents );
 	}
 
 	/**
